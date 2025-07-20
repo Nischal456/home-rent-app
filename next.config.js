627fe68a2b5b0a5757624cd2a94190b88b9be782
Nischal456 <shresthanischal456@gmail.com>
2025-07-20 14:52:45 +0545

    updatenow

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Your Next.js configurations can go here.
+  // We've fixed all the errors, so you don't need to ignore them anymore.
+};
+
+module.exports = nextConfig;
